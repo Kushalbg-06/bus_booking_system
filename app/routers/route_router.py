2014a69e0bd2bc53c077d8d7c8588ce7b0fb8e53
Kushal B G <kushalbg06@gmail.com>
2025-12-10 22:09:12 +0530

    add routes router

--- app/routers/route_router.py
+++ app/routers/route_router.py
@@ -1,10 +1,32 @@
 from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import models,schemas
+from .. import models
+from ..schemas import routes
 from typing import List
 
 router=APIRouter(
     prefix="/route",
     tags=["Route"]
 )
+ 
+@router.post("/",response_model=routes.RouteResponse)
+def add_routes(route:routes.RouteCreate,db:Session=Depends(get_db)):
+    new_route=models.Route(
+        origin=route.origin,
+        destination=route.destination,
+        distance=route.distance,
+        duration=route.duration
+    )
+    db.add(new_route)
+    db.commit()
+    db.refresh(new_route)
+
+    return new_route
+
+@router.get("/",response_model=List[routes.RouteResponse])
+def get_all_route(db:Session=Depends(get_db)):
+    routes=db.query(models.Route).all()
+    if not routes:
+        raise HTTPException(404,detail="routes not found")
+    return routes
