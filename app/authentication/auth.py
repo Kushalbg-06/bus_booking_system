53217e480c79e472303cdeb73de765f5710e5667
Kushal B G <kushalbg06@gmail.com>
2025-12-17 22:22:39 +0530

    update  auth

--- app/authentication/auth.py
+++ app/authentication/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models,schemas,hashing
+from ..schemas import users
 from . import token
 from fastapi.security import OAuth2PasswordRequestForm
 
@@ -27,3 +28,17 @@ def register(request:schemas.UserCreate,db:Session=Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+@router.post('/login',response_model=schemas.Token)
+def login(request:schemas.Login,db:Session=Depends(get_db)):
+    user=db.query(models.User).filter(models.User.email==request.email).first()
+    if not user:
+        raise HTTPException(404,detail="invalid email")
+   
+    if not hashing.verify_password(request.password, user.password):
+        raise HTTPException(404,detail="invalid password")
+     
+    access_token = token.create_access_token(data={"sub": str(user.id)}) 
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
