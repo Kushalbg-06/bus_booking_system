bf81e57ad669fcc23477a6f028eda9654f990242
Kushal B G <kushalbg06@gmail.com>
2025-12-30 22:16:37 +0530

    update booking router

--- app/routers/booking_router.py
+++ app/routers/booking_router.py
@@ -29,3 +29,18 @@ def my_bookings(db: Session = Depends(get_db), current = Depends(get_current_use
         raise HTTPException(403, "Admins have no user bookings")
     user = current["user"]
     return db.query(models.Booking).filter(models.Booking.user_id == user.id).all()
+
+@router.delete("/cancel/{booking_id}")
+def cancel_booking(booking_id: int, db: Session = Depends(get_db), current = Depends(get_current_user)):
+    if current["is_admin"]:
+        raise HTTPException(403, "Admins cannot cancel user bookings")
+    user = current["user"]
+    booking = db.query(models.Booking).filter(models.Booking.id == booking_id, models.Booking.user_id == user.id).first()
+    if not booking:
+        raise HTTPException(404, "Booking not found")
+    seat = db.query(models.Seat).filter(models.Seat.id == booking.seat_id).first()
+    if seat:
+        seat.is_booked = False
+    booking.status = "Cancelled"
+    db.commit()
+    return {"message": "Cancelled"}
